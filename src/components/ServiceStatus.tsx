a8fe613b70decfbf5fe7dfafba84d8b45e7cf527
rayan <eng.rayanhnide98@gmail.com>
2024-04-15 13:28:35 +0300

    final

---
+++ src/components/ServiceStatus.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+ 
+
+const ServiceStatus: React.FC = () => {
+
+ 
+ 
+
+
+  return (
+    <React.Fragment>
+      <h5>
+        Rayan Hnide Front-End Web Developer
+      </h5>
+    </React.Fragment>
+  );
+};
+
+export default ServiceStatus;
